9130b0ef2d640f9b7b3d69fe0e89fe5bc807f918
Bill Kilmer <wdk122@gmail.com>
2017-06-18 16:11:55 -0700

    Day 2

--- day2.2.js
+++ day2.2.js
@@ -27,14 +27,16 @@ find the bathroom code
 function codeFinder(input) {
   // initialize keycode
   let keycode = '';
-  // create keypad
+  // create keypad, use zeroes for non-keys
   const keypad = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
+    [0,   0,   1,   0, 0], 
+    [0,   2,   3,   4, 0],
+    [5,   6,   7,   8, 9],
+    [0, 'A', 'B', 'C', 0],
+    [0,   0, 'D',   0, 0]
   ];
   // initialize finger position [row, col] at 5:
-  const finger = [1, 1];
+  const finger = [2, 0];
 
 
   // iterate over each char in string
@@ -43,20 +45,20 @@ function codeFinder(input) {
     if (input[i] === 'R') {
       // move finger to the right
       // increment col by 1 if possible
-      if (finger[1] < 2) {
+      if (finger[1] < 4   && keypad[finger[0]][finger[1] + 1]) {
         finger[1]++;
       }
 
     } else if (input[i] === 'L') {
-      if (finger[1] !== 0) {
+      if (finger[1] !== 0 && keypad[finger[0]][finger[1] - 1]) {
         finger[1]--;
       }
     } else if (input[i] === 'U') {
-      if (finger[0] !== 0) {
+      if (finger[0] !== 0 && keypad[finger[0] - 1][finger[1]]) {
         finger[0]--;
       }
     } else if (input[i] === 'D') {
-      if (finger[0] < 2) {
+      if (finger[0] < 4   && keypad[finger[0] + 1][finger[1]]) {
         finger[0]++;
       }
 
